db2b30226bcfafdabcc1f3176f6f1b1212d44714
OfirDataloopAI <ofir.g@dataloop.ai>
2024-01-15 13:27:58 +0200

    added support for secrets

--- models/LicensePlateRecognition/model_adapter.py
+++ models/LicensePlateRecognition/model_adapter.py
@@ -12,8 +12,19 @@ logger = logging.getLogger('[Nvidia Models]')
 
 @dl.Package.decorators.module(name='model-adapter',
                               description='Model Adapter for NVIDIA TAO models',
-                              init_inputs={'model_entity': dl.Model})
+                              init_inputs={
+                                  'ngc_api_key_secret_name': 'String',
+                                  'ngc_org_secret_name': 'String',
+                                  'model_entity': dl.Model
+                              })
 class TaoModelAdapter(dl.BaseModelAdapter):
+    def __init__(self, ngc_api_key_secret_name, ngc_org_secret_name, model_entity: dl.Model = None):
+        self.ngc_config = {
+            "ngc_api_key": os.environ[ngc_api_key_secret_name],
+            "ngc_org": os.environ[ngc_org_secret_name],
+        }
+        super().__init__(model_entity=model_entity)
+
     def load(self, local_path, **kwargs):
         # Remove "downloading ngc" section when there is a working image with ngc-cli
         logger.info('downloading ngc')
@@ -32,16 +43,16 @@ class TaoModelAdapter(dl.BaseModelAdapter):
                                    stdout=subprocess.PIPE,
                                    stderr=subprocess.PIPE, shell=True)
         logger.info(f'Loading api configs from {os.getcwd()}: content: {os.listdir(os.getcwd())}')
-        with open(f'models/api_configs.json', 'r') as f:
-            _configuration = json.load(f)
+        # with open(f'models/api_configs.json', 'r') as f:
+        #     _configuration = json.load(f)
+        # inputdata = _configuration["ngc_api_key"].encode() + b'\n\n' + \
+        #             _configuration["ngc_org"].encode() + b'\n\n\n'
 
-        inputdata = _configuration["ngc_api_key"].encode() + b'\n\n' + \
-                    _configuration["ngc_org"].encode() + b'\n\n\n'
         # inputdata = self.configuration["ngc_api_key"].encode() + b'\n\n' + \
         #             self.configuration["ngc_org"].encode() + b'\n\n\n'
-        process.communicate(input=inputdata)
-        os.makedirs('/tmp/tao_models', exist_ok=True)
 
+        inputdata = self.ngc_config["ngc_api_key"].encode() + b'\n\n' + \
+                    self.ngc_config["ngc_org"].encode() + b'\n\n\n'
         logger.info('loading model')
         self.tao_model = None
         self.images_path = os.path.join(os.getcwd(), 'images')
